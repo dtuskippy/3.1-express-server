578e015f9d378e9bc48591a117b42b908a02e967
Alan Chelko <achelko@gmail.com>
2023-07-27 07:50:30 +0000

    Final edits

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+
+app.get('/', (req, res) => {
+  res.send('Hello, Gizmo is here!');
+})
+
+app.route('/book')
+  .get((req, res) => {
+    res.send('Get a random book')
+  })
+  .post((req, res) => {
+    res.send('Add a book')
+  })
+  .put((req, res) => {
+    res.send('Update the book')
+  })
+
+
+
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
